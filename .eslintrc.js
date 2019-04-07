46c78047a6a10de15e99db449b78134076ea4f4d
Fernando Moreira <nandomoreira.me@gmail.com>
2019-04-07 00:31:38 -0300

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,22 @@
 module.exports = {
-  extends: 'standard'
+  extends: 'standard',
+  env: {
+    browser: true,
+    es6: true,
+    jquery: true
+  },
+  globals: {
+    "$": true,
+    "jQuery": true,
+    "fetch": true,
+    "window": true,
+    "document": true
+  },
+  env: {
+    node: true
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
 }
