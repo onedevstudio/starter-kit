beb4e3c33b776a98712b4d974e4a7f2ed5f23c2c
Fernando Moreira <nandomoreira.me@gmail.com>
2019-07-20 20:31:39 -0300

    .browserslistrc and upgrade packages

--- tasks/config/webpack.js
+++ tasks/config/webpack.js
@@ -1,7 +1,7 @@
 import webpack from 'webpack'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
-import CleanWebpackPlugin from 'clean-webpack-plugin'
+import { CleanWebpackPlugin } from 'clean-webpack-plugin'
 import OfflinePlugin from 'offline-plugin'
 
 import { join, resolve } from 'path'
